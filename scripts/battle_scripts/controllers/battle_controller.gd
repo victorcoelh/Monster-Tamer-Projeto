81188d56cbd1651bd868e91f96286ce212d756e3
Victor Coelho <victorcoelho250602@gmail.com>
2024-01-25 00:45:52 -0300

    Integrated battle display and improved animations

--- scripts/battle_scripts/controllers/battle_controller.gd
+++ scripts/battle_scripts/controllers/battle_controller.gd
@@ -18,6 +18,7 @@ signal open_battle_menu(selected_pos: Vector2i)
 signal select_attack_target
 
 const TURN_INDICATOR = preload("res://scenes/user_interface/turn_indicator.tscn")
+const COMBAT_DISPLAY = preload("res://scenes/user_interface/combat_display.tscn")
 
 @onready var battle_selection_menu = $"../../UserInterface/BattleSelectionMenu"
 @onready var event_bus = $"../../EventBus"
@@ -55,16 +56,32 @@ func _show_turn_indicator(actor: String):
 func resolve_attack(attack: Callable, target_unit: BaseUnit, attacker: BaseUnit):
 	var hit_chance: float = attacker.get_hit_rate() - target_unit.get_avoid_rate()
 	var crit_chance: float = attacker.get_crit_rate()
+	var displays = display_battle_displays(target_unit, attacker)
 	
 	if (rng.randf() < hit_chance):
 		var damage: int = attack.call(attacker, target_unit)
-		damage *=- _get_damage_modifier(crit_chance, attacker)
+		damage *= _get_damage_modifier(crit_chance, attacker)
+		displays[1].reduce_health_by(damage)
+		
 		event_bus.unit_took_damage.emit(target_unit, damage)
 	else:
 		print("Attack Dodged!")
 	
-	await get_tree().create_timer(0.1).timeout
+	var waiting_time = displays[1].ANIMATION_TIME + 1
+	await get_tree().create_timer(waiting_time).timeout
+	displays.map(func(x): x.queue_free())
 	event_bus.unit_ended_turn.emit()
+
+func display_battle_displays(target_unit: BaseUnit, attacker: BaseUnit):
+	var attacker_display = COMBAT_DISPLAY.instantiate()
+	var target_display = COMBAT_DISPLAY.instantiate()
+	
+	attacker_display.set_unit(attacker)
+	target_display.set_unit(target_unit)
+	add_child(attacker_display)
+	add_child(target_display)
+	
+	return [attacker_display, target_display]
 
 func _get_damage_modifier(crit_chance: float, attacker: BaseUnit):
 	if (rng.randf() < crit_chance):
--- scripts/user_interface/combat_display.gd
+++ scripts/user_interface/combat_display.gd
@@ -1,36 +1,42 @@
-extends CanvasLayer
 class_name CombatDisplay
+extends CanvasLayer
+
 
 @onready var top_big_box = $Control/BigBox/Top
-
 @onready var dmg_top = $Control/AttributeBox/DMGBox/Top
 @onready var hit_top = $Control/AttributeBox/HITBox/Top
 @onready var crit_top = $Control/AttributeBox/CRITBox/Top
 
 @onready var unit_name = $Control/BigBox/UnitName
-
 @onready var dmg_count = $Control/DMGText/DMGCount
 @onready var hit_count = $Control/HITText/HITCount
 @onready var crit_count = $Control/CRITText/CRITCount
-
 @onready var health_bar = $Control/BigBox/HealthBar
 
 @onready var control = $Control
+
+var parent_unit: BaseUnit
+const DRAINING_MULTIPLIER := 100
+const ANIMATION_TIME := 2
 
 var color_scheme: Dictionary = {
 	'ALLY': Color(0.392157, 0.584314, 0.929412, 1),
 	'ENEMY': Color(0.933, 0.294, 0.169, 1)
 }
 
+
+func set_unit(unit: BaseUnit):
+	parent_unit = unit
+
 func _ready():
-	pass
+	set_display(parent_unit)
+	set_attributes(parent_unit)
 
 func set_display(unit: BaseUnit):
 	if unit is EnemyUnit:
 		change_box_color(color_scheme['ENEMY'])
-		control.anchor_left = 0.3
+		control.anchor_left = 0.7
 	if unit is PlayerUnit:
-
 		change_box_color(color_scheme['ALLY'])
 	
 	set_attributes(unit)
@@ -48,7 +54,12 @@ func set_attributes(unit: BaseUnit):
 	crit_count.text = str(unit.get_crit_rate() * 100)
 	
 	health_bar.min_value = 0
-	health_bar.max_value = unit.max_hp
-	health_bar.value = unit.hp
+	health_bar.max_value = unit.max_hp * DRAINING_MULTIPLIER
+	health_bar.value = unit.hp * DRAINING_MULTIPLIER
+
+func reduce_health_by(dmg: int):
+	var tween = create_tween()
+	var end_hp = health_bar.value - (dmg * DRAINING_MULTIPLIER)
 	
-	
+	tween.tween_property(health_bar, "value" , end_hp, ANIMATION_TIME)
+	tween.tween_callback(tween.kill)
